0d4bffe751be084bffb5aa696c423f4aee626fb2
damascenop <81495672+damascenop@users.noreply.github.com>
2023-04-21 14:57:09 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -21,6 +21,7 @@ joy.addEventListener("click", (e) => {
   buttons.style.display = "flex";
   formComplete.style.display = "none";
   h1.style.display = "none";
+  
 });
 
 form.addEventListener("click", (e) => {
@@ -33,18 +34,91 @@ home.addEventListener("click", (e) => {
   location.reload();
 });
 
-const botoes = document.querySelectorAll(".btn");
-const totalBtn = document.querySelector(".buttons");
-const anaS = document.querySelector("#AnaS");
+    const bt1 = document.getElementById("bt1");
+    const AnaS = document.getElementById("AnaS");
+    const allButtons = document.getElementById("buttons")
+    const AnaLuizaDiv = document.getElementById('AnaLuiza');
+    const AnaCDiv = document.getElementById('AnaC');
+    const ViniciusADiv = document.getElementById('ViniciusA');
+    const MarcosVDiv = document.getElementById('MarcosV');
+    const GabrielaSDiv = document.getElementById('GabrielaS');
+    const CamilaVDiv = document.getElementById('CamilaV');
+    const RuthCDiv = document.getElementById('RuthC');
+    const MariaCDiv = document.getElementById('MariaC');
+    const SidcleaFDiv = document.getElementById('SidcleaF');
+    const BiancaADiv = document.getElementById('BiancaA');
+    const EvelinRDiv = document.getElementById('EvelinR');
+    const GustavoADiv = document.getElementById('GustavoA');
+    const LuisFDiv = document.getElementById('LuisF');
+    
+    
 
-botoes.forEach((botao) => {
-  botao.addEventListener("click", function () {
-    totalBtn.style.display = "none";
-
-    switch (botao.innerText) {
-      case "Ana Sarah":
-        anaS.style.display = "block";
-        break;
-    }
+  bt1.addEventListener("click", () => {
+    allButtons.style.display = "none";
+    AnaS.style.display = "block";
   });
-});
+
+  bt2.addEventListener("click", () => {
+    allButtons.style.display = "none";
+    AnaLuiza.style.display = "block";
+  });
+  
+  bt3.addEventListener("click", () => {
+    allButtons.style.display = "none";
+    AnaC.style.display = "block";
+  });
+  
+  bt4.addEventListener("click", () => {
+    allButtons.style.display = "none";
+    ViniciusA.style.display = "block";
+  });
+  
+  bt5.addEventListener("click", () => {
+    allButtons.style.display = "none";
+    MarcosV.style.display = "block";
+  });
+  
+  bt6.addEventListener("click", () => {
+    allButtons.style.display = "none";
+    GabrielaS.style.display = "block";
+  });
+  
+  bt7.addEventListener("click", () => {
+    allButtons.style.display = "none";
+    CamilaV.style.display = "block";
+  });
+  
+  bt8.addEventListener("click", () => {
+    allButtons.style.display = "none";
+    RuthC.style.display = "block";
+  });
+  
+  bt9.addEventListener("click", () => {
+    allButtons.style.display = "none";
+    MariaC.style.display = "block";
+  });
+  
+  bt10.addEventListener("click", () => {
+    allButtons.style.display = "none";
+    SidcleaF.style.display = "block";
+  });
+  
+  bt11.addEventListener("click", () => {
+    allButtons.style.display = "none";
+    BiancaA.style.display = "block";
+  });
+  
+  bt12.addEventListener("click", () => {
+    allButtons.style.display = "none";
+    EvelinR.style.display = "block";
+  });
+  
+  bt13.addEventListener("click", () => {
+    allButtons.style.display = "none";
+    GustavoA.style.display = "block";
+  });
+  
+  bt14.addEventListener("click", () => {
+    allButtons.style.display = "none";
+    LuisF.style.display = "block";
+  });
